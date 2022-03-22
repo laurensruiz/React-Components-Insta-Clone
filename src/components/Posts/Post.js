3c0cfd16dfc8ea7a676487200bde33124cb93ce3
laurensruiz <laurenruiz97@yahoo.com>
2022-03-22 16:06:19 -0400

    fixed issues

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -5,7 +5,7 @@ import PostHeader from './PostHeader';
 
 const Post = props => {
   // 🔥 Make sure the parent of Post is passing the right props!
-  const { post, likePost } = props;
+  const { post, likePost } = props; //notice order of props
 
   return (
     <div className='post-border'>
@@ -21,7 +21,7 @@ const Post = props => {
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} numberOfLikes ={post.likes} />
+      <LikeSection likePost={() => likePost(post.id)} numberOfLikes ={post.likes} /> {/*first invocation of likepost*/}
       {/* Comments also wants its props! */}
       <Comments comments={post.comments}/>
     </div>
